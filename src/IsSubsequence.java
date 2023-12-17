7e33a80157afe21b18c33339cfc901507d5a8c0b
YuanYuan Yu <yuanyuanyu@gmail.com>
2023-12-18 01:18:24 +0800

    Small fix for "Is Subsequence"

--- src/IsSubsequence.java
+++ src/IsSubsequence.java
@@ -4,10 +4,7 @@
 // (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
 public class IsSubsequence {
-  public boolean isSubsequence(String s, String t) {
-    if (s.length() == 0) return true;
-    if (t.length() == 0) return false;
-    
+  public boolean isSubsequence(String s, String t) {    
     int i = 0, j = 0;
     while (i < s.length() && j < t.length()) {
       if (s.charAt(i) == t.charAt(j)) {
